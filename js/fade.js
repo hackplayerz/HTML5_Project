a1dd417ef415bc1e47dbfd543b78a7a6fc4af7d4
Alter0 <hackplayerz@users.noreply.github.com>
2018-05-24 20:06:39 +0900

    Add files via upload

---
+++ js/fade.js
@@ -0,0 +1,7 @@
+// FadeIn-Out Component
+$(document).ready(function(){
+    // to fade in on page load
+    $("body").css("display", "none");
+    $("body").fadeIn(2000); 
+    // to fade out before redirect
+})
